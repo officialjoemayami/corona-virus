e06492319d90487b94e0ca416975f33b1a2b731d
officialjoemayami <46620364+officialjoemayami@users.noreply.github.com>
2020-03-20 13:45:06 +0100

    update main file
    fixing api

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from source import google
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/extract')
+@app.route('/extract', methods=['GET'])
 def extract():
-    return google.extract()
+    if (request.method == 'GET'):
+        return jsonify(google.extract())
+    else:
+        request jsonify({'message': 'Wrong request method'})
